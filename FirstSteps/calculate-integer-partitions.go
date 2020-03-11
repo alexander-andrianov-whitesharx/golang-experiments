7c1d3f2c848e24861b0524b1d23965e67375daf0
Alexander <alex1der00@gmail.com>
2020-03-11 11:19:40 +0300

    add integer partition task

---
+++ FirstSteps/calculate-integer-partitions.go
@@ -0,0 +1,17 @@
+func Partitions(n int) int {
+	return CalculatePartitions(n, n)
+}
+
+func CalculatePartitions(currentSum int, maxValue int) int {
+	if currentSum == maxValue {
+		return 1 + CalculatePartitions(currentSum, maxValue - 1)
+	}
+	if maxValue == 0 || currentSum < 0 {
+		return 0
+	}
+	if maxValue == 1 || currentSum == 0 {
+		return 1
+	}
+  
+	return CalculatePartitions(currentSum, maxValue - 1) + CalculatePartitions(currentSum - maxValue, maxValue)
+}
